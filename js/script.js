f9402491d37f21c2abd0f2d49818cb58160e3899
Izabela Janusz <afrodyzjak100@gmail.com>
2022-04-10 21:36:20 +0200

    changes js2

--- js/script.js
+++ js/script.js
@@ -2,52 +2,53 @@
     const welcome = () => {
         console.log("Cześć to już raz było robione ale cóz😅");
     }
-    
+    const removeButton = () => {
+
+        const photo = document.querySelector(".js-photo");
+        const removePhotoButton = document.querySelector(".js-removePhotoButton");
+        photo.classList.toggle("hidden");
+        removePhotoButton.innerText = photo.classList.contains("hidden") ? "Przywróć zdjęcie" : "Usuń zdjęcie";
+    };
+
+    const remove = () => {
+        const removePhotoButton = document.querySelector(".js-removePhotoButton");
+
+        removePhotoButton.addEventListener("click", removeButton);
+
+
+    };
+
     const buttonClick = () => {
         const body = document.querySelector(".js-body");
         const themeName = document.querySelector(".js-themeName");
-        
+
         body.classList.toggle("dark");
-            if (body.classList.contains("dark")) {
-                themeName.innerText = "jasny";
-            } else {
-                themeName.innerText = " ciemny"; 
-            }
+        if (body.classList.contains("dark")) {
+            themeName.innerText = "jasny";
+        } else {
+            themeName.innerText = " ciemny";
+        }
     }
 
     const init = () => {
-       const button = document.querySelector(".js-button");
-       
-        
+        const button = document.querySelector(".js-button");
+
+
         button.addEventListener("click", buttonClick);
     };
 
-const removeButton = () => {
-    const removePhotoButton = document.querySelector(".js-removePhotoButton");
-    const photo = document.querySelector(".js-photo");
 
-    photo.classList.toggle("hidden");
-    removePhotoButton.innerText = photo.classList.contains("hidden") ? "Przywróć zdjęcie" : "Usuń zdjęcie";
-};
-
-const remove = () => {
-  
-    removePhotoButton.addEventListener("click", removeButton); 
-
+    welcome();
     remove();
-};
+    init();
 
-   
-welcome();
-init();
-    
-    
-   
-    
-    
-    
-    
-  
-   
-    
+
+
+
+
+
+
+
+
+
 };
